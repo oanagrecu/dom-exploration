241bfc24a9a36e85ed26afbaa6a682760a095140
oanagrecu <oanagrecu09@yahoo.com>
2023-06-06 15:32:17 +0200

    resolving DOM Selectors exercices

--- selectors/script.js
+++ selectors/script.js
@@ -1,2 +1,33 @@
-let important = document.querySelector('.important');
-important.setAttribute("title", "This is an important item");
+var importantItems = document.querySelectorAll(".important");
+console.log(importantItems);
+for (let item of importantItems) {
+  item.setAttribute("title", "This is an important item.");
+  console.log(item);
+}
+
+var images = document.querySelectorAll("img");
+
+for (let image of images) {
+  let value = image.getAttribute("class");
+  if (value !== "important") {
+    image.style.display = "none";
+  }
+}
+var paragraphsElts = document.querySelectorAll("p");
+const randomRgbColor = () => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + "," + g + "," + b + ")";
+};
+
+for (paragraph of paragraphsElts) {
+  let className = paragraph.getAttribute("class");
+  if (className !== null) {
+    console.log(className);
+    console.log(paragraph.innerText);
+  } else {
+    paragraph.style.color = randomRgbColor();
+    console.log(paragraph.innerText);
+  }
+}
