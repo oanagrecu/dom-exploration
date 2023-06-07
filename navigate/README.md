4d8a116b0a21663d118fe138521dc3ecbaef6088
oanagrecu <oanagrecu09@yahoo.com>
2023-06-07 18:14:22 +0200

    resolved Navigate the DOM tree tasks

---
+++ navigate/README.md
@@ -0,0 +1,6 @@
+## Exercises
+
+1.Modify the script.js, do not create any new nodes!
+2.Select the last child of the <ol> tag and put it at the beginning of the list
+3.Move the <h2> of the third section in the second one and vice-versa
+4.Delete the last section from the DOM, we don't need it anyways
---
+++ navigate/script.js
@@ -0,0 +1,28 @@
+// Modify the script.js, do not create any new nodes!
+
+// TASK 1 - Select the last child of the <ol> tag and put it at the beginning of the list
+
+const orderedList = document.querySelector("ol");
+// Get the last child of the <ol> element
+var lastChild = orderedList.lastElementChild;
+// Move the last child to the beginning of the list
+orderedList.insertBefore(lastChild, orderedList.firstChild);
+
+// TASK 2 - Move the <h2> of the third section in the second one and vice-versa
+var secondSection = document.querySelectorAll("section")[1];
+var thirdSection = document.querySelectorAll("section")[2];
+
+var secondSectionHeading = secondSection.querySelector("h2");
+var thirdSectionHeading = thirdSection.querySelector("h2");
+secondSection.appendChild(thirdSectionHeading);
+thirdSection.appendChild(secondSectionHeading);
+
+//TASK 3 - Delete the last section from the DOM, we don't need it anyways
+// var lastSection = document.querySelector("section:last-of-type");
+// lastSection.parentNode.removeChild(lastSection);
+
+var thirdSection = document.querySelectorAll("section")[2];
+thirdSection.parentNode.removeChild(thirdSection);
+
+// let sections = document.querySelectorAll("section");
+// sections[sections.length - 1].remove();
